f74c95d57226c2cf8f9cdae98d98b64114dd666a
hmiftachul11 <hmiftachul11@gmail.com>
2024-01-19 00:56:13 +0700

    Add Hooks For Session Strorage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        let username = sessionStorage.getItem("username");
+        if (username === '' || username === null) {
+            navigate('/login');
+        }
+    })
   return (
     <>
       <div className="container">
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -8,45 +8,53 @@ const LoginPage = () => {
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    sessionStorage.clear();
+  });
+
   const login = (e) => {
     e.preventDefault();
     if (validation()) {
-      fetch("http://localhost:8000/user/"+username).then((res)=>{
-        return res.json();
-      }).then((resp)=>{
+      fetch("http://localhost:8000/user/" + username)
+        .then((res) => {
+          return res.json();
+        })
+        .then((resp) => {
           console.log(resp);
-          if(Object.keys(resp).length === 0){
-            toast.error('Please enter valid username');
+          if (Object.keys(resp).length === 0) {
+            toast.error("Please enter valid username");
           } else {
-            if(resp.password === password){
-                toast.success('Login Success');
-                navigate("/");
+            if (resp.password === password) {
+              toast.success("Login Success");
+              sessionStorage.setItem("username", username);
+              navigate("/");
             } else {
-              toast.error('Please enter valid credentials');
+              toast.error("Please enter valid credentials");
             }
           }
-      }).catch((err)=>{
+        })
+        .catch((err) => {
           toast.error("Login Failed due to : " + err.message);
-      })
+        });
     }
   };
   const validation = () => {
     let result = true;
     if (username === "" || username === null) {
-      result=false;
+      result = false;
       toast.warning("Please enter username");
     }
     if (password === "" || password === null) {
-      result=false;
+      result = false;
       toast.warning("Please enter password");
     }
     return result;
-  }
+  };
   return (
     // <div className="container">
     //   Login Page
     //   <br />
-    //   
+    //
     // </div>
     <div className="row">
       <div className="offset-lg-3 col-lg-6">
@@ -60,18 +68,31 @@ const LoginPage = () => {
                 <label>
                   User Name <span className="errmsg">*</span>
                 </label>
-                <input value={username} onChange={(e) => usernameUpdate(e.target.value)} className="form-control" />
+                <input
+                  value={username}
+                  onChange={(e) => usernameUpdate(e.target.value)}
+                  className="form-control"
+                />
               </div>
               <div className="form-group">
                 <label>
                   Password <span className="errmsg">*</span>
                 </label>
-                <input value={password} onChange={(e) => passwordUpdate(e.target.value)} className="form-control" type="password" />
+                <input
+                  value={password}
+                  onChange={(e) => passwordUpdate(e.target.value)}
+                  className="form-control"
+                  type="password"
+                />
               </div>
             </div>
             <div className="card-footer">
-              <button type="submit" className="btn btn-primary">Submit</button>
-              <Link to="/register" className="btn btn-success float-end">Register</Link>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+              <Link to="/register" className="btn btn-success float-end">
+                Register
+              </Link>
             </div>
           </div>
         </form>
